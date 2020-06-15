4bce468398c5ec191c7d6814b241a71977ba3dd5
Ilnaz Minniakhmetov <i.minniahmetov.stc@innopolis.university>
2020-06-15 17:55:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите первое число");
+        Scanner scanner = new Scanner(System.in);
+        int type = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int type2 = scanner.nextInt();
+        if (type < type2)
+        System.out.println("Минимальное число из введенных - " + type);
+        else if (type > type2)
+        System.out.println("Минимальное число из введенных - " + type2);
+
+        }
+    }
