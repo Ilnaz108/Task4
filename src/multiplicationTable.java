61c81b3b8c5784bc6c6d86e34a0985180a947360
Ilnaz108 <66380719+Ilnaz108@users.noreply.github.com>
2020-06-17 06:55:05 +0300

    Update multiplicationTable.java

--- src/multiplicationTable.java
+++ src/multiplicationTable.java
@@ -1,6 +1,6 @@
 public class multiplicationTable {
-    public static void main(String... args){
-        for(int i = 1; i < 11; i++){
+    public static void main(String... args) {
+        for (int i = 1; i < 11; i++){
             for(int k = 1; k < 11; k++){
                 System.out.print(k * i + " ");
             }
